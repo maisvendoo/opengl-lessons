a5ae72232179fee9415e07f57ee0000d39d8e0d1
maisvendoo <maisvendoo@yandex.ru>
2022-07-21 02:03:25 +0300

    В уроке 1.3 добавлены комменты и очистка цветового буфера

--- hello/src/main.cpp
+++ hello/src/main.cpp
@@ -18,11 +18,15 @@ void resize_callback(GLFWwindow *window, int width, int height)
 //------------------------------------------------------------------------------
 int main()
 {
+    // Инициализируем GLFW
     glfwInit();
+    // Устанавливаем используемую версию OpenGL и профиль
+    // (OpenGL 3.3 Core Profile)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    // Создаем экземпляр окна приложения
     GLFWwindow *window = glfwCreateWindow(1280,
                                           720,
                                           "Hello, modern OpenGL!",
@@ -36,22 +40,36 @@ int main()
         return -1;
     }
 
+    // Создаем контекст отображения (контекст окна, связанный с OpenGL)
     glfwMakeContextCurrent(window);
 
+    // Инициализируем GLAD - загрузчик указателей на требуемые нам
+    // функции OpenGL
     if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
     {
         std::cout << "Error: Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
+    // Устанавливаем функкцию обратного вызова (callback)
+    // регулизующую размер области отрисовки, при изменении
+    // размеров окна
     glfwSetFramebufferSizeCallback(window, resize_callback);
 
+    // Главный цикл приложения
     while (!glfwWindowShouldClose(window))
     {
+        // Заполняем цветовой буфер заданным цветом
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Переключаем экранный буфер (двойная буферизация)
         glfwSwapBuffers(window);
+        // Обработка событий
         glfwPollEvents();
     }
 
+    // Освобождаем ресурсы, занятые GLFW
     glfwTerminate();
 
     return 0;
