cc6c44a4efe5b2e33d43e03348b35c56d15e3ecd
maisvendoo <maisvendoo@yandex.ru>
2022-07-21 03:03:22 +0300

    Нарисовал треугольник

---
+++ triangle/src/main.cpp
@@ -0,0 +1,170 @@
+#include    <glad/glad.h>
+#include    <GLFW/glfw3.h>
+
+#include    <iostream>
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+const char* vertexShaderSource =
+
+        "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+const char* fragmentShaderSource =
+
+        "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\0";
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void resize_callback(GLFWwindow *window, int width, int height)
+{
+    (void) window;
+
+    glViewport(0, 0, width, height);
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+int main()
+{
+    // Инициализируем GLFW
+    glfwInit();
+    // Устанавливаем используемую версию OpenGL и профиль
+    // (OpenGL 3.3 Core Profile)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Создаем экземпляр окна приложения
+    GLFWwindow *window = glfwCreateWindow(1280,
+                                          720,
+                                          "Hello, modern OpenGL!",
+                                          nullptr,
+                                          nullptr);
+
+    if (window == nullptr)
+    {
+        std::cout << "Error: Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Создаем контекст отображения (контекст окна, связанный с OpenGL)
+    glfwMakeContextCurrent(window);
+
+    // Инициализируем GLAD - загрузчик указателей на требуемые нам
+    // функции OpenGL
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+    {
+        std::cout << "Error: Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Устанавливаем функкцию обратного вызова (callback)
+    // регулизующую размер области отрисовки, при изменении
+    // размеров окна
+    glfwSetFramebufferSizeCallback(window, resize_callback);
+
+    // Задаем массив вершин
+    GLfloat vertices[] = {
+      -0.5f, -0.5f, 0.0f,
+       0.5f, -0.5f, 0.0f,
+       0.0f,  0.5f, 0.0f
+    };
+
+    // Создаем объект буфера вершин (Vertex Buffer OBject - VBO)
+    GLuint VBO, VAO;
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // Выгружаем данные вершин в видеоустройство
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void *) 0);
+    glEnableVertexAttribArray(0);
+
+    // Создаем вершинный шейдер и выполняем его компиляцию
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    GLint success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        std::cout << "ERROR: Vertex shader: " << infoLog << std::endl;
+    }
+
+    // Создаем фрагментный шейдер и компилируем его
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        std::cout << "ERROR: Fragment shader: " << infoLog << std::endl;
+    }
+
+    // Компонуем шейдеры в шейдерную программу
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cout << "ERROR: Link shader program: " << infoLog << std::endl;
+    }
+
+    // Главный цикл приложения
+    while (!glfwWindowShouldClose(window))
+    {
+        // Заполняем цветовой буфер заданным цветом
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // Переключаем экранный буфер (двойная буферизация)
+        glfwSwapBuffers(window);
+        // Обработка событий
+        glfwPollEvents();
+    }
+
+    // Уничтожаем шейдеры
+    glDeleteShader(fragmentShader);
+    glDeleteShader(vertexShader);
+
+    // Освобождаем ресурсы, занятые GLFW
+    glfwTerminate();
+
+    return 0;
+}
